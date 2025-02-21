f5c082b744ee6c1a31616120c6b70ff459f26dbe
AndDev <23031429@itcelaya.edu.mx>
2025-02-20 19:54:51 -0600

    update dependencies and configuration for React support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+
 import react from "@astrojs/react";
+
 
 // https://astro.build/config
 export default defineConfig({
